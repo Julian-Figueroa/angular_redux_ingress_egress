4623cba78d3d4382634b8db0ac9cf48b2c09be79
Julian Figueroa <julianfigueroatorres@gmail.com>
2019-06-15 11:01:03 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngressEgressComponent } from './ingress-egress/ingress-egress.component';
+import { StatisticComponent } from './ingress-egress/statistic/statistic.component';
+import { DetailComponent } from './ingress-egress/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngressEgressComponent,
+    StatisticComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
